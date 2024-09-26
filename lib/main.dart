6aaaeb0a337bed84a089a8ca856f1cc3854e1ca3
Alfiya6282823310 <knalfiya@gmail.com>
2024-09-26 23:21:11 +0530

    created

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(Patient());
+}
+
+class Patient extends StatelessWidget {
+  const Patient({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
